85e55f53d3c297bab83a7aee30b346b8ce7bd26a
evtin <evtin@ya.ru>
2024-08-09 22:37:43 +0300

    dev

--- src/pages/Dashboard/CountHospitals/CountHospitals.tsx
+++ src/pages/Dashboard/CountHospitals/CountHospitals.tsx
@@ -1,25 +1,41 @@
-import Badge from 'components/base/Badge';
 import { Card } from 'react-bootstrap';
 import Chart from './Chart';
+import { useEffect, useState } from 'react';
+import { reportHospitalCountForTheYear } from '../../../services/ReportService';
 
 const CountHospitals = () => {
+  const [data, setData] = useState([]);
+  const [total, setTotal] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await reportHospitalCountForTheYear();
+
+      if (typeof response?.data === 'object') {
+        setData(response.data);
+        const sum: number = response.data.reduce(
+          (acc: number, curr: number) => acc + curr,
+          0
+        );
+        setTotal(sum.toString());
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Card className="h-100">
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
-            <h5 className="mb-1">
-              New customers
-              <Badge bg="warning" variant="phoenix" pill className="ms-2">
-                +26.5%
-              </Badge>
-            </h5>
-            <h6 className="text-body-tertiary">Last 7 days</h6>
+            <h5 className="mb-1">Количество стационаров</h5>
+            <h6 className="text-body-tertiary">за последний год</h6>
           </div>
-          <h4>356</h4>
+          <h4>{total}</h4>
         </div>
         <div className="pb-0 pt-4">
-          <Chart />
+          <Chart data={data} />
         </div>
       </Card.Body>
     </Card>
